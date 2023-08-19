853e13da19b93b236e33639704eb7dad19d09993
Drew Deaton <drewbdeaton@gmail.com>
2023-08-19 12:32:09 -0500

    Document motion

--- include/rev/api/alg/drive/motion/motion.hpp
+++ include/rev/api/alg/drive/motion/motion.hpp
@@ -1,5 +1,9 @@
 #include "rev/api/odometry/odometry.hpp"
 
+/**
+ * @brief Interface for generating raw motor powers
+ * 
+ */
 class Motion {
   virtual std::tuple<double, double> genPowers(OdometryState current_state,
                                                Position target_state) = 0;
