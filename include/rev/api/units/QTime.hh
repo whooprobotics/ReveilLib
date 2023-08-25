90066f416cd45c2e51564237102535e75d7488d8
Drew Deaton <drewbdeaton@gmail.com>
2023-08-25 13:21:57 -0500

    Rename units files to reflect project style

--- include/rev/api/units/QTime.hh
+++ include/rev/api/units/QTime.hh
@@ -1,7 +1,11 @@
 /*
- * This code is a modified version of Benjamin Jurke's work in 2015. You can read his blog post
+ * This code is a modified version of OkapiLib's units library, which in turn is a modified version of
+ * Benjamin Jurke's work in 2015. You can read his blog post
  * here:
  * https://benjaminjurke.com/content/articles/2015/compile-time-numerical-unit-dimension-checking/
+ *
+ * You can find the OkapiLib code this was taken from here:
+ * https://github.com/purduesigbots/OkapiLib
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
