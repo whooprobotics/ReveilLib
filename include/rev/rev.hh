dbe73b0df72f7d0cc177ffeef1114e4f6605af50
Drew Deaton <drewbdeaton@gmail.com>
2023-08-28 16:26:27 -0500

    Adapt odometry skeleton to use new units

--- include/rev/rev.hh
+++ include/rev/rev.hh
@@ -2,4 +2,7 @@
 #include "rev/api/alg/drive/motion/motion.hh"
 #include "rev/api/alg/odometry/odometry.hh"
 #include "rev/api/hardware/chassis/chassis.hh"
-#include "rev/api/unit/unit.hh"
+
+#include "rev/api/unit/unit.hh" // Deprecated legacy units
+
+#include "rev/api/units/all_units.hh"
