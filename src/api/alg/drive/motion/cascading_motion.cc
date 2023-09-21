4c2aeab397c6999102948fdb5ec7c6483e23d860
Drew Deaton <drewbdeaton@gmail.com>
2023-09-20 21:42:25 -0500

    Implement Cascading Motion Constructor

--- src/api/alg/drive/motion/cascading_motion.cc
+++ src/api/alg/drive/motion/cascading_motion.cc
@@ -4,6 +4,13 @@
 #include <algorithm>
 #include <cmath>
 
+rev::CascadingMotion::CascadingMotion(double ipower,
+                                      double ik_p,
+                                      double ik_b,
+                                      QSpeed imax_v,
+                                      double ik_v)
+    : power(ipower), k_p(ik_p), k_b(ik_b), max_v(imax_v), k_v(ik_v) {}
+
 std::tuple<double, double> rev::CascadingMotion::gen_powers(
     rev::OdometryState current_state,
     rev::Position target_state,
@@ -12,12 +19,17 @@ std::tuple<double, double> rev::CascadingMotion::gen_powers(
   // Calculate the absolute angle from the robot's facing direction to the
   // target point
   QAngle angle_to_target = atan2(target_state.x - current_state.pos.x,
-                                target_state.y - current_state.pos.y);
+                                 target_state.y - current_state.pos.y);
   // Calculate the difference between where the robot is facing and that angle
   QAngle err_a = current_state.pos.facing - angle_to_target;
   QLength distance_to_target =
-      std::sqrt(std::pow(target_state.x.convert(inch) - current_state.pos.x.convert(inch), 2) +
-           std::pow(target_state.y.convert(inch) - current_state.pos.y.convert(inch), 2)) * inch;
+      std::sqrt(std::pow(target_state.x.convert(inch) -
+                             current_state.pos.x.convert(inch),
+                         2) +
+                std::pow(target_state.y.convert(inch) -
+                             current_state.pos.y.convert(inch),
+                         2)) *
+      inch;
 
   // Scale down distance to just get the longitudinal component
   // apply drop_early term
@@ -28,9 +40,12 @@ std::tuple<double, double> rev::CascadingMotion::gen_powers(
 
   // Get longitudinal speed
   // Its just the dot product of the velocity vector and the facing unit vector
-  QSpeed v = current_state.vel.xv * cos(current_state.pos.facing) + current_state.vel.yv * sin(current_state.pos.facing);
+  QSpeed v = current_state.vel.xv * cos(current_state.pos.facing) +
+             current_state.vel.yv * sin(current_state.pos.facing);
 
-  double finalPower = (k_p * (v_target - v).convert(inch / second) + v_target.convert(inch / second) * k_v) * sgn(power);
+  double finalPower = (k_p * (v_target - v).convert(inch / second) +
+                       v_target.convert(inch / second) * k_v) *
+                      sgn(power);
 
   finalPower = std::clamp(finalPower, -power, power);
 
