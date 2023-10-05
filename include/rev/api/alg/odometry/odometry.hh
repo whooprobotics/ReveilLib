6778cc4450e2b1a6a6e588bece686280e77f02ed
Drew Deaton <drewbdeaton@gmail.com>
2023-10-05 13:54:13 -0500

    Add documentation to the odometry interface

--- include/rev/api/alg/odometry/odometry.hh
+++ include/rev/api/alg/odometry/odometry.hh
@@ -1,9 +1,9 @@
 #pragma once
-#include "rev/api/units/r_quantity.hh"
-#include "rev/api/units/q_length.hh"
 #include "rev/api/units/q_angle.hh"
-#include "rev/api/units/q_speed.hh"
 #include "rev/api/units/q_angular_speed.hh"
+#include "rev/api/units/q_length.hh"
+#include "rev/api/units/q_speed.hh"
+#include "rev/api/units/r_quantity.hh"
 
 namespace rev {
 struct Position {
@@ -24,8 +24,22 @@ struct OdometryState {
 };
 
 class Odometry {
+  /**
+   * @brief Get the current position and velocity of the robot
+   *
+   * @return OdometryState
+   */
   virtual OdometryState get_state() = 0;
+  /**
+   * @brief Set the position of the controller
+   *
+   * @param pos
+   */
   virtual void set_position(Position pos) = 0;
+  /**
+   * @brief Set the position to (0,0,0)
+   *
+   */
   virtual void reset_position() = 0;
 };
 }  // namespace rev
