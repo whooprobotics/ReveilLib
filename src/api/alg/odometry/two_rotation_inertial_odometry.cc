c403d81c72cc057b03cb5777409d7cce5d1d64c7
Drew Deaton <drewbdeaton@gmail.com>
2023-10-11 21:57:56 -0500

    Create thread-safe set_position function

--- src/api/alg/odometry/two_rotation_inertial_odometry.cc
+++ src/api/alg/odometry/two_rotation_inertial_odometry.cc
@@ -2,12 +2,21 @@
 
 namespace rev {
 OdometryState TwoRotationInertialOdometry::get_state() {
-  current_position_mutex.take(20);
+  current_position_mutex.take(TIMEOUT_MAX);
   OdometryState ret = current_position;
   current_position_mutex.give();
   return ret;
 }
-void TwoRotationInertialOdometry::set_position(Position pos) {}
+void TwoRotationInertialOdometry::set_position(Position pos) {
+  current_position_mutex.take(TIMEOUT_MAX);
+
+  current_position.pos = pos;
+  current_position.vel = {0 * inch / second, 0 * inch / second};
+
+  inertial.set_yaw(pos.facing.convert(degree));
+
+  current_position_mutex.give();
+}
 void TwoRotationInertialOdometry::reset_position() {}
 void TwoRotationInertialOdometry::step() {}
 }  // namespace rev
