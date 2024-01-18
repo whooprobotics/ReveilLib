d5defd092395e63ddb639237ace2740663791ba0
walk3rc <61813096+walk3rc@users.noreply.github.com>
2024-01-17 20:35:26 -0600

    Start of brining okapi tbh controller over to reveil
    need to convert some okapi things over to its reveil counterpart

--- include/rev/api/alg/tbh/tbh.hh
+++ include/rev/api/alg/tbh/tbh.hh
@@ -2,77 +2,62 @@
 #include "rev/api/async/async_runnable.hh"
 #include "rev/api/units/all_units.hh"
 
-pros::Motor motor_1(1);
-pros::Motor_Group motor_group({motor_1});
 namespace rev {
 
 class TBH_Controller : public AsyncRunnable {
  public:
-  TBH_Controller();  // Constructor function / declare arguments..?
+  TBH_Controller(pros::MotorGroup& imotor_group,
+                 double& ikI);  // Constructor function /
+                                // declare arguments..?
 
-  void setMotorPower(double power) {
-    // set power to motor in voltage
-    motor_group.move_voltage(
-        power);  // is this right...? //
-                 // ########################################################################
-  }
+  double step(double inewReading);  // for async
 
-  ENCODERTYPE_IDK getMotorEncoder() {
-    // returns encoder value
-    return motor_group.get_encoder_units();
-  }
+  void setTarget(double itarget);
 
-  void setVelocity() {}
+  void controllerSet(double ivalue);
 
-  void calculateSpeed() {
-    // get current encoder value
-    // compare to last retrieved encoder value and time from last
-    // save current encoder and time
-    // calculate rpm from previous and current time/encoder
-  }
+  double getTarget();
 
-  void updateVoltageTBH() {
-    // do the crazy math stuff
-  }
+  double getProcessValue();
 
-  void step() override;  // needed for Async
+  double getOutput();
+
+  double getMaxOutput();
+
+  double getMinOutput();
+
+  double getError();
+
+  bool isSettled();
+
+  void setSampleTime(okapi::QTime isampleTime);  // ################## NEEDS THE
+                                                 // REVEIL VERSION..?
+
+  void setOutputLimits(double imax, double imin);
+
+  void setControllerSetTargetLimits(double itargetMax, double itargetMin);
+
+  void reset();
+
+  void flipDisable();
+
+  void flipDisable(bool iisDisabled);
+
+  bool isDisabled();
 
  private:  // declare all variables
-           // https://gist.github.com/jpearman/6761b53919101f07c400
-// Update inteval (in mS) for the flywheel control loop
-#define FW_LOOP_SPEED = 1
+  pros::MotorGroup* motor_group;
+  double kI;
 
-// Maximum power we want to send to the flywheel motors
-#define FW_MAX_POWER = 1
-
-  long counter;  ///< loop counter used for debug
-
-  // encoder tick per revolution
-  float ticks_per_rev;  ///< encoder ticks per revolution
-
-  // Encoder
-  long e_current;  ///< current encoder count
-  long e_last;     ///< current encoder count
-
-  // velocity measurement
-  float v_current;  ///< current velocity in rpm
-  long v_time;      ///< Time of last velocity calculation
-
-  // TBH control algorithm variables
-  long target;          ///< target velocity
-  long current;         ///< current velocity
-  long last;            ///< last velocity
-  float error;          ///< error between actual and target velocities
-  float last_error;     ///< error last time update called
-  float gain;           ///< gain
-  float drive;          ///< final drive out of TBH (0.0 to 1.0)
-  float drive_at_zero;  ///< drive at last zero crossing
-  long first_cross;     ///< flag indicating first zero crossing
-  float drive_approx;   ///< estimated open loop drive
-
-  // final motor drive
-  long motor_drive;  ///< final motor control value
-
-  // #############################################################################
+  double error{0};
+  double target{0};
+  double output{0};
+  double outputSum{0};
+  double takeBackHalf{0};
+  double outputMax{1};
+  double outputMin{-1};
+  double controllerSetTargetMax{1};
+  double controllerSetTargetMin{-1};
+  bool controllerIsDisabled{false};
 };
 };  // namespace rev
