580a3c450fb3d23a6998e21b557a3391c34c74d6
dimi1729 <dimi@tamu.edu>
2025-01-13 20:23:51 -0600

    starting doing opencv stuff

---
+++ include/rev/util/plotting/plotting_opencv.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include "rev/api/alg/odometry/odometry.hh"
+#include "rev/api/units/q_angle.hh"
+#include "rev/api/units/q_length.hh"
+#include "rev/util/math/point_vector.hh"
+#include "rev/util/math/pose.hh"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace rev;
+
+int test(){
+    cv::Mat image = cv::imread("field.jpg")
+}
