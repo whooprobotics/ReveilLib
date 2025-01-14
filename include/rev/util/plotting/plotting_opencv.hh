ed541df4f2ca25368fc68925b3c4edbb419144e2
dimi1729 <dimi@tamu.edu>
2025-01-13 21:13:43 -0600

    fixed include for opencv

--- include/rev/util/plotting/plotting_opencv.hh
+++ include/rev/util/plotting/plotting_opencv.hh
@@ -1,4 +1,5 @@
 #pragma once
+#ifdef OFF_ROBOT_TESTS
 
 #include <tuple>
 #include <vector>
@@ -7,11 +8,19 @@
 #include "rev/api/units/q_length.hh"
 #include "rev/util/math/point_vector.hh"
 #include "rev/util/math/pose.hh"
-#include <opencv2/opencv.hpp>
 #include <iostream>
+
+#include <opencv2/opencv.hpp>
 
 using namespace rev;
 
 int test(){
-    cv::Mat image = cv::imread("field.jpg")
+    cv::Mat image = cv::imread("field.png", cv::IMREAD_COLOR);
+    if (image.empty()) {
+        std::cerr << "Could not read the image: " << std::endl;
+        return 1;
+    }
+    return 0;
 }
+
+#endif
