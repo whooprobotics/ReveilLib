6b525a1e1ef9bb43a2cc8175bd0fa3a69c868e02
Drew <drewbdeaton@gmail.com>
2025-03-21 20:55:51 -0500

    Rough boomerang implementation

---
+++ src/api/alg/reckless/boomerang.cc
@@ -0,0 +1,82 @@
+#include "rev/api/alg/reckless/boomerang.hh"
+#include <iostream>
+#include "rev/api/alg/drive/stop/stop.hh"
+#include "rev/api/alg/odometry/odometry.hh"
+#include "rev/api/alg/reckless/path.hh"
+namespace rev {
+
+void BoomerangSegment::init(OdometryState initial_state) {
+  std::cout << "Boomerang segment invoked" << std::endl;
+  this->start_point = initial_state.pos;
+}
+
+SegmentStatus BoomerangSegment::step(OdometryState current_state) {
+
+  QLength d_to_start = sqrt((start_point.x - current_state.pos.x) *
+                                (start_point.x - current_state.pos.x) +
+                            (start_point.y - current_state.pos.y) *
+                                (start_point.y - current_state.pos.y));
+
+  QLength current_d = sqrt((current_state.pos.x - target_point.x) *
+                               (current_state.pos.x - target_point.x) +
+                           (current_state.pos.y - target_point.y) *
+                               (current_state.pos.y - target_point.y));
+
+  stop_state new_state;
+  if(close)
+    new_state = this->stop->get_stop_state(current_state, target_point,
+                                                    frozen_carrot_point, drop_early);
+  else {
+    new_state = stop_state::GO;
+
+    if(current_d < 7.5_in) {
+      close = true;
+      frozen_carrot_point = current_state.pos;
+    }
+  }
+
+  // Calculate carrot point
+  QLength carrot_x = target_point.x - lead * current_d * cos(target_point.theta);
+  QLength carrot_y = target_point.y - lead * current_d * sin(target_point.theta);
+
+  Position carrot_point (carrot_x, carrot_y, 0_deg);
+
+
+  this->part_progress = d_to_start.convert(inch) / (d_to_start + current_d).convert(inch);
+
+  // Prevent status from regressing
+  if (last_status.status == SegmentStatusType::NEXT ||
+      new_state == stop_state::EXIT)
+    return last_status = SegmentStatus::next();
+
+  if (last_status.status == SegmentStatusType::BRAKE ||
+      new_state == stop_state::BRAKE)
+    return last_status = SegmentStatus::brake();
+
+  std::tuple<double, double> pows = this->motion->gen_powers(
+      current_state, carrot_point, this->start_point, this->drop_early);
+
+  // Handle coasting if needed
+  if (new_state == stop_state::COAST) {
+    double power = this->stop->get_coast_power();
+    double left, right;
+    std::tie(left, right) = pows;
+    if (left + right < 0)
+      power *= -1;
+    return last_status = SegmentStatus::drive(power);
+  }
+  // Apply correction
+  std::tuple<double, double> corrected_pows =
+      this->correction->apply_correction(current_state, carrot_point,
+                                         this->start_point, this->drop_early,
+                                         pows);
+  return SegmentStatus::drive(corrected_pows);
+}
+
+void BoomerangSegment::clean_up() {}
+
+double BoomerangSegment::progress() {
+  return part_progress;
+}
+
+}  // namespace rev
