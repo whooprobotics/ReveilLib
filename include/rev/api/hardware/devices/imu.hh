b5337e219030d6c1f607e33fc9cb19f73e73ff93
sean-m32 <77125011+sean-m32@users.noreply.github.com>
2025-03-24 14:39:48 -0500

    Revert "removed unused imu header thing"
    This reverts commit 0d40b7bad74bd0a444ec32384d7a6e9823f194d5.
    
    forgot develop can't be directly committed to lol

---
+++ include/rev/api/hardware/devices/imu.hh
@@ -0,0 +1,17 @@
+#pragma once
+#include "pros/imu.hpp"
+#include "rev/api/hardware/devices/gyroscope/gyroscope.hh"
+
+namespace rev {
+
+class Imu : Gyroscope {
+ public:
+  Imu(int port);
+  double get_position();
+  bool is_calibrating();
+
+ private:
+  pros::Imu inertial;
+};
+
+}  // namespace rev
