e01fcb5cd448dd6b427e78cfffc0a53c3658f439
sean-m32 <77125011+sean-m32@users.noreply.github.com>
2025-09-24 20:46:23 -0500

    updated power

--- include/rev/api/alg/slipstream/power.hh
+++ include/rev/api/alg/slipstream/power.hh
@@ -2,17 +2,14 @@ namespace rev {
 
 struct SlipstreamPower {
   double front_left_forward;
-  double front_left_steer;
-
   double front_right_forward;
-  double front_right_steer;
-
   double rear_left_forward;
-  double rear_left_steer;
-
   double rear_right_forward;
-  double rear_right_steer;
 
+  double front_left_steer = 0.0;
+  double front_right_steer = 0.0;
+  double rear_left_steer = 0.0;
+  double rear_right_steer = 0.0;
 };
 
 } // namespace rev
