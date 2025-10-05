c5aa5273c5ac9ac5e38d38bb3dde0cbfa66cb6ec
sean-m32 <77125011+sean-m32@users.noreply.github.com>
2025-10-05 17:52:07 -0500

    added comments to header files + formatting in SkidSteerChassis

--- include/rev/api/hardware/chassis/mecanum_chassis.hh
+++ include/rev/api/hardware/chassis/mecanum_chassis.hh
@@ -15,33 +15,51 @@ class MecanumChassis : public HolonomicChassis {
   /**
    * @brief Construct a new MecanumChassis object
 
-   * @param front_left 
+   * @param front_left A motor (group) of the front left chassis motor(s).
    * 
-   * @param back_left
+   * @param back_left A motor (group) of the back left chassis motor(s).
    * 
-   * @param front_right
+   * @param front_right A motor (group) of the front right chassis motor(s).
    * 
-   * @param back_right
+   * @param back_right A motor (group) of the back right chassis motor(s).
    *
    */
-  MecanumChassis(rev::AnyMotor& ifront_left, rev::AnyMotor& iback_left, rev::AnyMotor& ifront_right, rev::AnyMotor& iback_right);
+  MecanumChassis(rev::AnyMotor& ifront_left, rev::AnyMotor& iback_left, 
+                 rev::AnyMotor& ifront_right, rev::AnyMotor& iback_right);
   
+  /**
+   * @brief Drive the robot in a tank drive style
+   */
   void drive_tank(double leftv, double rightv) override;
+
+  /**
+   * @brief Heuristic function for a tank drive with arcade joystick controls
+   */
   void drive_arcade(double forward, double yaw) override;
+
+  /**
+   * @brief Drive the robot in a mecanum drive style
+   */
   void drive_holonomic(SlipstreamPower power) override;
+
+  /**
+   * @brief Heuristic function for a holonomic drive from joystick controls
+   */
   void drive_holonomic(double forward, double yaw, double strafe) override;
   
   /**
-   * @brief Sets the brake types of all motors to brake
+   * @brief Sets the brake types of all chassis motors to brake
    */
   void set_brake_harsh() override;
-   /**
-    * @brief Sets the brake types of all motors to coast
-    */
+
+  /**
+   * @brief Sets the brake types of all chassis motors to coast
+   */
   void set_brake_coast() override;
-   /**
-    * @brief Stops all of the motors
-    */
+
+  /**
+   * @brief Stops all chassis motors
+   */
   void stop() override;
 
  private:
