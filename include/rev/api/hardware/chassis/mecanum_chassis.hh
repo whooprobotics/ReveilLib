dd5c773bcfd89782f6a5937463c68cd8418a56e7
Abhilash Sunkara <abhilaash.sunkara@gmail.com>
2025-10-05 16:57:25 -0500

    Initial setup for mecanum drive

---
+++ include/rev/api/hardware/chassis/mecanum_chassis.hh
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "api.h"
+#include "rev/api/hardware/chassis/chassis.hh"
+#include "rev/api/hardware/motor/any_motor.hh"
+
+namespace rev {
+/**
+ * @brief Implementation of a skid-steer chassis
+ *
+ */
+class MecanumChassis : public Chassis {
+ public:
+  /**
+   * @brief Construct a new Mecanum Chassis
+   *
+   * @param frontleft A motor group of the front left side chassis motors. Applying a
+   * positive voltage to these should cause the left side of the chassis to move
+   * forward.
+   * @param frontright A motor group of the front right side chassis motors. Applying a
+   * positive voltage to these should cause the right side of the chassis to
+   * move forward.
+   * @param backleft A motor group of the back left side chassis motors. Applying a
+   * positive voltage to these should cause the left side of the chassis to move
+   * forward.
+   * @param backright A motor group of the back right side chassis motors. Applying a
+   * positive voltage to these should cause the right side of the chassis to
+   * move forward.
+   */
+  MecanumChassis(rev::AnyMotor& ifrontleft, rev::AnyMotor& ifrontright, rev::AnyMotor& ibackleft, rev::AnyMotor& ibackright);
+
+  void drive(double horizontal_v, double vertical_v, double angular_v);
+
+  /**
+   * @brief Sets the brake types of all motors to brake
+   */
+  void set_brake_harsh() override;
+  /**
+   * @brief Sets the brake types of all motors to coast
+   */
+  void set_brake_coast() override;
+  /**
+   * @brief Stops all of the motors
+   */
+  void stop() override;
+
+ private:
+  rev::AnyMotor* frontleft;
+  rev::AnyMotor* frontright;
+  rev::AnyMotor* backleft;
+  rev::AnyMotor* backright;
+};
+}  // namespace rev
---
+++ src/api/hardware/chassis/mecanum_chassis.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cstdlib>
+
+#include "rev/api/hardware/chassis/mecanum_chassis.hh"
+
+namespace rev {
+MecanumChassis::MecanumChassis(rev::AnyMotor& ifrontleft, rev::AnyMotor& ifrontright, rev::AnyMotor& ibackleft, rev::AnyMotor& ibackright)
+    : frontleft(&ifrontleft), frontright(&ifrontright), backleft(&ibackleft), backright(&ibackright) {}
+
+void MecanumChassis::drive(double horizontal_v, double vertical_v, double angular_v) {
+  /* horizontal_v = std::clamp(horizontal_v, -1.0, 1.0);
+  vertical_v = std::clamp(vertical_v, -1.0, 1.0);
+  angular_v = std::clamp(angular_v, -1.0, 1.0); */
+
+  double denominator = std::max(std::abs(horizontal_v), std::abs(vertical_v), std::abs(angular_v));
+
+  double front_left_power = (vertical_v + horizontal_v + angular_v) / denominator;
+  double front_right_power = (vertical_v - horizontal_v - angular_v) / denominator;
+  double back_left_power = (vertical_v - horizontal_v + angular_v) / denominator;
+  double back_right_power = (vertical_v + horizontal_v - angular_v) / denominator;
+
+  frontleft->move_voltage(12000 * front_left_power);
+  frontright->move_voltage(12000 * front_left_power);
+  backleft->move_voltage(12000 * front_left_power);
+  backright->move_voltage(12000 * front_left_power);
+}
+
+void MecanumChassis::set_brake_harsh() {
+  frontleft->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
+  frontright->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
+  backleft->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
+  backright->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_BRAKE);
+}
+
+void MecanumChassis::set_brake_coast() {
+  frontleft->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_COAST);
+  frontright->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_COAST);
+  backleft->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_COAST);
+  backright->set_brake_mode(pros::motor_brake_mode_e::E_MOTOR_BRAKE_COAST);
+}
+
+void MecanumChassis::stop() {
+  frontleft->brake();
+  frontright->brake();
+  backleft->brake();
+  backright->brake();
+}
+}  
