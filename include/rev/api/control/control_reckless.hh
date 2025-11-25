c0b8430ab8b33d4f1b85ee588601989ab58ca909
EthanMik <etmik@icloud.com>
2025-11-25 01:12:11 -0600

    Added more drive motions

--- include/rev/api/control/control_reckless.hh
+++ include/rev/api/control/control_reckless.hh
@@ -1,5 +1,7 @@
 struct DriveParams;
 struct TurnToAngleParams;
+struct TurnToPointParams;
+struct StopParams;
 
 class ControlReckless {
 public:
@@ -28,19 +30,40 @@ public:
     ControlReckless();
 
     void drive_to_point(float x, float y, const DriveParams& p);
-    void turn_to_angle(float angle_deg, const TurnToAngleParams& p);
+    void drive_to_angle(float x, float y, float angle, const DriveParams& p);    
+    
+    void turn_to_angle(float angle, const TurnToAngleParams& p);
+    void turn_to_point(float x, float y, const TurnToPointParams& p);
+
 };
 
 extern ControlReckless control;
 
-struct DriveParams{
+struct StopParams {
+    float harsh_threshold_ms = control.stop_harsh_threshold_ms;
+    float coast_threshold_ms = control.stop_coast_threshold_ms;
+    float coast_power = control.stop_coast_power;
+    float timeout_ms = control.stop_timeout_ms;
+};
+
+struct DriveParams {
     float max_power = control.drive_max_power;
     float correction = control.drive_correction;
     float max_error = control.drive_max_error;
+    float drop_early = 0;
 };
 
-struct TurnToAngleParams{
+struct TurnToAngleParams {
     float max_power = control.turn_max_power;
+    float coast_power = control.turn_coast_power;
+    float harsh_coeff = control.turn_harsh_coeff;
+    float coast_coeff = control.turn_coast_coeff;
+    float brake_time_ms = control.turn_brake_time_ms;
+};
+
+struct TurnToPointParams {
+    float max_power = control.turn_max_power;
+    float angle_offset = 0;
     float coast_power = control.turn_coast_power;
     float harsh_coeff = control.turn_harsh_coeff;
     float coast_coeff = control.turn_coast_coeff;
