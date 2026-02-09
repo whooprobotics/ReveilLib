ba91d93124851863cc553bb2cdf80ab07192cce5
sean-m32 <77125011+sean-m32@users.noreply.github.com>
2026-02-09 12:52:13 -0600

    modified RC workflow to set name for copro package

--- scripts/package_copro.sh
+++ scripts/package_copro.sh
@@ -3,4 +3,4 @@
 mkdir copro_package 
 find include -name "*.hh" | grep -E '(common|copro)' | xargs -I {} cp --parents {} copro_package
 cp bin/reveillib_copro.so copro_package
-zip -r copro_package.zip copro_package
+zip -r reveillib_copro.zip copro_package
